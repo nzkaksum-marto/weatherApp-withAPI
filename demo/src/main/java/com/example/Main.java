5e7649e72d8a4699422724886c8034091693ccb7
nzkaksum-marto <martinmiroslavovpetrov@gmail.com>
2025-07-02 19:34:31 +0300

    Working

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,24 +1,31 @@
 package com.example;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-import java.io.InputStream;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) {
+        String apiKey = "API_KEY"; // Replace with your WeatherAPI key
+        String city = "Madrid";
+        String url = "https://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + city + "&aqi=no";
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .build();
+
         try {
-            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("API.json");
-            if (inputStream == null) {
-                throw new RuntimeException("API.json not found in resources!");
-            }
-            StudentList studentList = objectMapper.readValue(inputStream, StudentList.class);
-            for (Student student : studentList.getStudent()) {
-                System.out.print("  Student name: " + student.getStudentName());
-                System.out.print("  Student ID: " + student.getStudentId());
-                System.out.println("  Course: " + student.getCourse());
-            }
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response.body());
+            System.out.println("City: " + jsonNode.get("location").get("name").asText());
+            System.out.println("Temperature: " + jsonNode.get("current").get("temp_c").asDouble() + "°C");
+            System.out.println("Condition: " + jsonNode.get("current").get("condition").get("text").asText());
+            System.out.println("UV:" +jsonNode.get("current").get("uv").asText());
         } catch (Exception e) {
             e.printStackTrace();
         }
